e39b8e20ff5d62b72711d370fd91e10947dc4443
rtimagawa <45678750+rtimagawa@users.noreply.github.com>
2018-12-08 11:54:43 -0200

    home

--- AULAS/06.py
+++ AULAS/06.py
@@ -2,4 +2,5 @@ n1 = float(input('Digite um numero '))
 n2 = float(input('Digite outro numero '))
 
 r=n1+n2
-print('A soma é ',r)
+#print('A soma entre ',n1,' e ',n2,' é ',r)
+print('A soma entre {} e {} vale {}'.format(n1,n2,r))
