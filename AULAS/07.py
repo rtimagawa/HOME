5a69c61d03698d720882b398d75600ee580e6f08
rtimagawa <45678750+rtimagawa@users.noreply.github.com>
2018-12-08 13:02:50 -0200

    home

---
+++ AULAS/07.py
@@ -0,0 +1,16 @@
+#Operadores logicos
+
+n1=int(input('digite n1 '))
+n2=int(input('digite n2'))
+
+s = n1+n2
+m = n1*n2
+d = n1/n2
+di = n1//n2
+e = n1**n2
+
+print('a soma é {}'.format(s))
+print('a multiplicacao é {}'.format(m))
+print('a divisao é {}'.format(d))
+print('a divisao inteira é {}'.format(di))
+print('a potencia é {}'.format(e))
